6cafb49f8b3d04f8a59a20d00dab033aed886f27
abdulkuddusa4 <abdulkuddusa4@gmail.com>
2023-05-10 07:03:11 +0600

    the title of the window has been changed

--- TheFlappyBird/desktop/src/com/mygdx/game/DesktopLauncher.java
+++ TheFlappyBird/desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("My GDX Game");
+		config.setTitle("The Flappy Bird");
 		config.setWindowedMode(1000,650);
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
